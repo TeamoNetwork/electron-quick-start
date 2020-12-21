218684d26650304113e9b29f4b54c98cd017a30b
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2020-12-20 16:12:27 -0800

    Updated main.js for localhost use

--- main.js
+++ main.js
@@ -5,15 +5,16 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1400,
+    height: 900,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  // mainWindow.loadFile('index.html')
+   mainWindow.loadURL("http://localhost:3000")
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -24,7 +25,7 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-  
+
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
